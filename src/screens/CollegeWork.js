039938d12a668b9189d1a9065e64b9960f61f3f0
alissav <63168432+alissav30@users.noreply.github.com>
2020-08-21 18:15:59 -0400

    quiz screens first commit

---
+++ src/screens/CollegeWork.js
@@ -0,0 +1,157 @@
+import React, {useState} from "react";
+import { Text, StyleSheet, Button, TouchableHighlight, View, Image } from "react-native";
+import { Dimensions, Platform, PixelRatio } from 'react-native';
+import normalize from "../../normalizeSize.js";
+import {TouchableOpacity} from "react-native-gesture-handler";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Ionicons } from '@expo/vector-icons'; 
+import { createStackNavigator, createAppContainer } from 'react-navigation';  
+
+
+export default function CollegeWork( props ) {
+    const [collegeWork, setCollegeWork] = useState('')
+    const [firstButtonPress, setFirstButtonPress] = useState('unclicked')
+    const [secondButtonPress, setSecondButtonPress] = useState('unclicked')
+    const [thirdButtonPress, setThirdButtonPress] = useState('unclicked')
+
+    const first_button = firstButtonPress === 'clicked'? styles.clickedButton : styles.unclickedButton
+    const first_text = firstButtonPress === 'clicked'? styles.clickedText : styles.unclickedText
+    const second_button = secondButtonPress === 'clicked'? styles.clickedButton : styles.unclickedButton
+    const second_text = secondButtonPress === 'clicked'? styles.clickedText : styles.unclickedText
+    const third_button = thirdButtonPress === 'clicked'? styles.clickedButton : styles.unclickedButton
+    const third_text = thirdButtonPress === 'clicked'? styles.clickedText : styles.unclickedText
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.questionText}>
+                How much are you thinking of working in college?
+            </Text>
+            <View style={styles.questions}>
+                <View style={styles.question}>
+                    <TouchableHighlight 
+                        style={first_button}
+                        onPress={() => 
+                            {if (firstButtonPress === 'unclicked'){
+                                setFirstButtonPress('clicked')
+                                setSecondButtonPress('unclicked')
+                                setThirdButtonPress('unclicked')
+                                setCollegeWork('$26,000 or less')
+                            }
+                            {if (firstButtonPress === 'clicked') {
+                                setFirstButtonPress('unclicked')
+                            }
+                        }}
+                        }>
+                        <Text style={first_text}>20 hours/week or more</Text>   
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.question}>
+                    <TouchableHighlight 
+                        style={second_button}
+                        onPress={() => 
+                            {if (secondButtonPress === 'unclicked'){
+                                setSecondButtonPress('clicked')
+                                setFirstButtonPress('unclicked')
+                                setThirdButtonPress('unclicked')
+                                setCollegeWork('$26,001 to $92,100')
+                            }
+                            {if (secondButtonPress === 'clicked') {
+                                setSecondButtonPress('unclicked')
+                            }
+                        }}
+                        }>
+                        <Text style={second_text}>Less than 20 hours/week</Text>   
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.question}>
+                    <TouchableHighlight 
+                        style={third_button}
+                        onPress={() => 
+                            {if (thirdButtonPress === 'unclicked'){
+                                setThirdButtonPress('clicked')
+                                setSecondButtonPress('unclicked')
+                                setFirstButtonPress('unclicked')
+                                setCollegeWork('More than $92,000')
+                            }
+                            {if (thirdButtonPress === 'clicked') {
+                                setThirdButtonPress('unclicked')
+                            }
+                        }}
+                        }>
+                        <Text style={third_text}>Work Study Program</Text>   
+                </TouchableHighlight>
+                </View>
+            </View>
+            <View style={styles.arrow}>
+                <TouchableOpacity
+                    onPress={() => 
+                        props.navigation.navigate("FamIncome2")
+                    }
+                >
+                    <Ionicons name="ios-arrow-round-forward" size={normalize(120)} color="black" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+  
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: "space-between",
+        paddingTop: normalize(70),
+    },
+    questions: {
+        paddingTop: normalize(20),
+        paddingBottom: normalize(35),
+        flex: 1
+    },
+    question: {
+        paddingTop: normalize(10),
+        paddingBottom: normalize(10),
+    },
+    questionText: {
+        fontSize: 35,
+        textAlign: 'center',
+        paddingRight: normalize(5),
+        paddingLeft: normalize(5)
+    },
+    unclickedText: {
+        color:'#A32934',
+        textAlign:'center',
+        fontSize: 25
+    },
+    clickedText: {
+        color:'#fff',
+        textAlign:'center',
+        fontSize: 25
+    },
+    clickedButton: {
+        marginRight:20,
+        marginLeft:20,
+        marginTop:10,
+        paddingTop:30,
+        paddingBottom:30,
+        borderRadius:80,
+        borderWidth: 10,
+        borderColor: '#A32934',
+        backgroundColor: '#A32934'
+    },
+    unclickedButton: {
+        marginRight:20,
+        marginLeft:20,
+        marginTop:10,
+        paddingTop:30,
+        paddingBottom:30,
+        borderRadius:80,
+        borderWidth: 10,
+        borderColor: '#A32934'
+    },
+    arrow: {
+        alignItems: 'flex-end',
+        marginRight: normalize(10)
+    }
+  });
+  
+  
---
+++ src/screens/EnrollmentType.js
@@ -0,0 +1,157 @@
+import React, {useState} from "react";
+import { Text, StyleSheet, Button, TouchableHighlight, View, Image } from "react-native";
+import { Dimensions, Platform, PixelRatio } from 'react-native';
+import normalize from "../../normalizeSize.js";
+import {TouchableOpacity} from "react-native-gesture-handler";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Ionicons } from '@expo/vector-icons'; 
+import { createStackNavigator, createAppContainer } from 'react-navigation';  
+
+
+export default function EnrollmentType( props ) {
+    const [enrollmentType, setEnrollmentType] = useState('')
+    const [firstButtonPress, setFirstButtonPress] = useState('unclicked')
+    const [secondButtonPress, setSecondButtonPress] = useState('unclicked')
+    const [thirdButtonPress, setThirdButtonPress] = useState('unclicked')
+
+    const first_button = firstButtonPress === 'clicked'? styles.clickedButton : styles.unclickedButton
+    const first_text = firstButtonPress === 'clicked'? styles.clickedText : styles.unclickedText
+    const second_button = secondButtonPress === 'clicked'? styles.clickedButton : styles.unclickedButton
+    const second_text = secondButtonPress === 'clicked'? styles.clickedText : styles.unclickedText
+    const third_button = thirdButtonPress === 'clicked'? styles.clickedButton : styles.unclickedButton
+    const third_text = thirdButtonPress === 'clicked'? styles.clickedText : styles.unclickedText
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.questionText}>
+                What type of college enrollment are you considering?
+            </Text>
+            <View style={styles.questions}>
+                <View style={styles.question}>
+                    <TouchableHighlight 
+                        style={first_button}
+                        onPress={() => 
+                            {if (firstButtonPress === 'unclicked'){
+                                setFirstButtonPress('clicked')
+                                setSecondButtonPress('unclicked')
+                                setThirdButtonPress('unclicked')
+                                setEnrollmentType('$26,000 or less')
+                            }
+                            {if (firstButtonPress === 'clicked') {
+                                setFirstButtonPress('unclicked')
+                            }
+                        }}
+                        }>
+                        <Text style={first_text}>Full-Time</Text>   
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.question}>
+                    <TouchableHighlight 
+                        style={second_button}
+                        onPress={() => 
+                            {if (secondButtonPress === 'unclicked'){
+                                setSecondButtonPress('clicked')
+                                setFirstButtonPress('unclicked')
+                                setThirdButtonPress('unclicked')
+                                setEnrollmentType('$26,001 to $92,100')
+                            }
+                            {if (secondButtonPress === 'clicked') {
+                                setSecondButtonPress('unclicked')
+                            }
+                        }}
+                        }>
+                        <Text style={second_text}>Half-Time</Text>   
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.question}>
+                    <TouchableHighlight 
+                        style={third_button}
+                        onPress={() => 
+                            {if (thirdButtonPress === 'unclicked'){
+                                setThirdButtonPress('clicked')
+                                setSecondButtonPress('unclicked')
+                                setFirstButtonPress('unclicked')
+                                setEnrollmentType('More than $92,000')
+                            }
+                            {if (thirdButtonPress === 'clicked') {
+                                setThirdButtonPress('unclicked')
+                            }
+                        }}
+                        }>
+                        <Text style={third_text}>Less than Half-Time</Text>   
+                </TouchableHighlight>
+                </View>
+            </View>
+            <View style={styles.arrow}>
+                <TouchableOpacity
+                    onPress={() => 
+                        props.navigation.navigate("CollegeWork")
+                    }
+                >
+                    <Ionicons name="ios-arrow-round-forward" size={normalize(120)} color="black" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+  
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: "space-between",
+        paddingTop: normalize(70),
+    },
+    questions: {
+        paddingTop: normalize(20),
+        paddingBottom: normalize(35),
+        flex: 1
+    },
+    question: {
+        paddingTop: normalize(10),
+        paddingBottom: normalize(10),
+    },
+    questionText: {
+        fontSize: 35,
+        textAlign: 'center',
+        paddingRight: normalize(5),
+        paddingLeft: normalize(5)
+    },
+    unclickedText: {
+        color:'#A32934',
+        textAlign:'center',
+        fontSize: 25
+    },
+    clickedText: {
+        color:'#fff',
+        textAlign:'center',
+        fontSize: 25
+    },
+    clickedButton: {
+        marginRight:20,
+        marginLeft:20,
+        marginTop:10,
+        paddingTop:30,
+        paddingBottom:30,
+        borderRadius:80,
+        borderWidth: 10,
+        borderColor: '#A32934',
+        backgroundColor: '#A32934'
+    },
+    unclickedButton: {
+        marginRight:20,
+        marginLeft:20,
+        marginTop:10,
+        paddingTop:30,
+        paddingBottom:30,
+        borderRadius:80,
+        borderWidth: 10,
+        borderColor: '#A32934'
+    },
+    arrow: {
+        alignItems: 'flex-end',
+        marginRight: normalize(10)
+    }
+  });
+  
+  
